e22c522127a454499eea57cd18a6ea3bb459d288
quyp <1095698022@qq.com>
2021-10-27 18:47:40 +0800

    feat: mine

---
+++ dist/pages/mine/index.js
@@ -0,0 +1,74 @@
+require('./../../runtime.js');
+require('./../../remax-vendors.js');
+(wx["webpackJsonp"] = wx["webpackJsonp"] || []).push([["pages/mine/index"],{
+
+/***/ "./src/pages/mine/index.css":
+/*!**********************************!*\
+  !*** ./src/pages/mine/index.css ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./src/pages/mine/index.css?modules":
+/*!******************************************!*\
+  !*** ./src/pages/mine/index.css?modules ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+module.exports = {"text":"text___2ex0Z","header":"header___2IhNK"};
+
+/***/ }),
+
+/***/ "./src/pages/mine/index.entry.js":
+/*!***************************************!*\
+  !*** ./src/pages/mine/index.entry.js ***!
+  \***************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _remax_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @remax/runtime */ "./node_modules/@remax/runtime/esm/index.js");
+/* harmony import */ var _index_tsx__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.tsx */ "./src/pages/mine/index.tsx");
+__webpack_require__(/*! ./index.css */ "./src/pages/mine/index.css");
+
+
+
+Page(Object(_remax_runtime__WEBPACK_IMPORTED_MODULE_0__["createPageConfig"])(_index_tsx__WEBPACK_IMPORTED_MODULE_1__["default"], 'pages/mine/index'));
+
+/***/ }),
+
+/***/ "./src/pages/mine/index.tsx":
+/*!**********************************!*\
+  !*** ./src/pages/mine/index.tsx ***!
+  \**********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var remax_wechat__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! remax/wechat */ "./node_modules/remax/wechat.js");
+/* harmony import */ var _index_css_modules__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./index.css?modules */ "./src/pages/mine/index.css?modules");
+/* harmony import */ var _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_index_css_modules__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__["createElement"](remax_wechat__WEBPACK_IMPORTED_MODULE_1__["View"], {
+    className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.app
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__["createElement"](remax_wechat__WEBPACK_IMPORTED_MODULE_1__["View"], {
+    className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.header
+  }, "\u4E2A\u4EBA\u4E2D\u5FC3"));
+});
+
+/***/ })
+
+},[["./src/pages/mine/index.entry.js","runtime","remax-vendors"]]]);
--- src/app.config.ts
+++ src/app.config.ts
@@ -1,10 +1,29 @@
 import { AppConfig } from "remax/wechat";
 
 const config: AppConfig = {
-  pages: ["pages/index/index"],
+  pages: ["pages/index/index", "pages/mine/index"],
   window: {
-    navigationBarTitleText: "title",
-    navigationBarBackgroundColor: "#4e77ef",
+    navigationBarTitleText: "",
+    navigationBarBackgroundColor: "#ffffff",
+  },
+  tabBar: {
+    color: "#999999",
+    selectedColor: "#4e77ef",
+    backgroundColor: "red",
+    list: [
+      {
+        pagePath: "pages/index/index",
+        iconPath: "/images/home.png",
+        selectedIconPath: "/images/home-hover.png",
+        text: "首页",
+      },
+      {
+        pagePath: "pages/mine/index",
+        iconPath: "/images/user.png",
+        selectedIconPath: "/images/user-hover.png",
+        text: "我的",
+      },
+    ],
   },
 };
 
