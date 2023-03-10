0eb8ce1e6675193c8f6965b2d3436d7635c3e5c3
quyp <1095698022@qq.com>
2023-03-10 14:57:53 +0800

    update

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,9 +1,22 @@
 export default defineAppConfig({
   pages: ["pages/index/index", "pages/me/index"],
   tabBar: {
+    color: "#3f3f6f",
+    selectedColor: "#6f68f9",
+    backgroundColor: "#101028",
     list: [
-      { pagePath: "pages/index/index", text: "首页" },
-      { pagePath: "pages/me/index", text: "我的" },
+      {
+        pagePath: "pages/index/index",
+        text: "首页",
+        iconPath: "assets/icon1.png",
+        selectedIconPath: "assets/icon1_s.png",
+      },
+      {
+        pagePath: "pages/me/index",
+        text: "我的",
+        iconPath: "assets/icon.png",
+        selectedIconPath: "assets/icon.png",
+      },
     ],
   },
   window: {
